bdfc5ceccbc5f6170da613ea9aa3ca67fd7c521d
robinrrr10 <robinrrr10@gmail.com>
2019-10-14 22:58:48 +0530

    added searc, people etc

--- playda/urls.py
+++ playda/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
-    path('admin/', admin.site.urls),
+    path('people/', include('people.urls')),
+    path('search/', include('search.urls')),
+    path('admin/', admin.site.urls)
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
